b8147a0786909a6a4b86422829c9da82848f92ac
Lucas Gunn <mail@lgunn.com>
2016-05-10 10:16:04 +1000

    Forgot to set data - might actually work now

--- src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
+++ src/android/io/github/pwlin/cordova/plugins/fileopener2/FileOpener2.java
@@ -94,6 +94,7 @@ public class FileOpener2 extends CordovaPlugin {
 				Uri path = Uri.fromFile(file);
 				Intent intent = new Intent(Intent.ACTION_VIEW);
 				// intent.setDataAndType(path, contentType);
+				intent.setData(path);
 				intent.putExtra(Intent.EXTRA_STREAM, path);
 				intent.setType(contentType);
 				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
